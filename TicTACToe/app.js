af04770381792407c69f68450b4c3fad3c2624f3
Pankaj Ugale <pankajugale2020@gmail.com>
2025-09-26 00:02:48 +0530

    Update app.js

--- TicTACToe/app.js
+++ TicTACToe/app.js
@@ -5,7 +5,7 @@ let newGameBtn = document.querySelector("#new-btn");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
 
-let turnO = true; //playerX, playerO
+let turnO = true; 
 let count = 0; //To Track Draw
 
 const winPatterns = [
